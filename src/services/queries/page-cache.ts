a5eddd73ce3377ae0629065c77c368e2889529d5
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-03 10:08:15 +0900

    feat: redis를 이용한 페이지 캐싱

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,21 @@
-export const getCachedPage = (route: string) => {};
+import { client } from "$services/redis";
 
-export const setCachedPage = (route: string, page: string) => {};
+const casheRoutes = [
+    '/about', '/privacy', '/auth/signin', '/auth/signup'
+];
+
+export const getCachedPage = (route: string) => {
+    if (casheRoutes.includes(route)) {
+        return client.get('pagecashe#' + route);
+    }
+
+    return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if (casheRoutes.includes(route)) {
+        return client.set('pagecashe#' + route, page, {
+            EX: 2
+        })
+    }
+};
