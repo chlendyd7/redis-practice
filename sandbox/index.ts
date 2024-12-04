7d54fae5bee5d04af25e9e3c76007b0a3da348b2
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-12-04 15:07:33 +0900

    feat: redis pipline 실습
    python은 client.pipline()
    로직 작성 후 client.execute()로 전송

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,21 +2,25 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-    await client.hSet('car', {
+    await client.hSet('car1', {
         color: 'red',
         year: 1950,
-        engine: { cylinders: 8},
-        owner: null || '',
-        service: undefined || ''
     });
-    
-    const car = await client.hGetAll('car#123232');
+    await client.hSet('car2', {
+        color: 'green',
+        year: 1960,
+    });
+    await client.hSet('car3', {
+        color: 'blue',
+        year: 1970,
+    });
 
-    if (Object.keys(car).length === 0) {
-        console.log('Car not found, respond with 404');
-        return;
-    }
+    const commands = [1,2,3].map((id) => {
+        return client.hGetAll('car' + id);
+    });
 
-    console.log(car);
+    const result = await Promise.all(commands);
+
+    console.log(result);
 };
 run();
