49ce55c980db21ae8eb6a2f69036f9e00ab1deac
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-12-04 15:17:22 +0900

    feat: getItemds pipline 구현

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -3,10 +3,25 @@ import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
 import { itemsKey } from '$services/keys';
+import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {};
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+    const commands = ids.map((id) => {
+        return client.hGetAll(itemsKey(id));
+    });
+
+    const results = await Promise.all(commands);
+
+    results.map((result, i) => {
+        if (Object.keys(result).length == 0) {
+            return null;
+        }
+
+        return deserialize(ids[i], result);
+    });
+};
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
     const id = genId();
