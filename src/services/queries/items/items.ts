ed29dc03acc30466a13cf5ff1ef3ead11c3b6890
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-05 17:20:20 +0900

    feat: deserialize 적용

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -5,30 +5,38 @@ import { genId } from '$services/utils';
 import { itemsKey } from '$services/keys';
 import { deserialize } from './deserialize';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+	const item = await client.hGetAll(itemsKey(id));
 
-export const getItems = async (ids: string[]) => {
-    const commands = ids.map((id) => {
-        return client.hGetAll(itemsKey(id));
-    });
+	if (Object.keys(item).length === 0) {
+		return null;
+	}
 
-    const results = await Promise.all(commands);
-
-    results.map((result, i) => {
-        if (Object.keys(result).length == 0) {
-            return null;
-        }
-
-        return deserialize(ids[i], result);
-    });
+	return deserialize(id, item);
 };
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
-    const id = genId();
-    
-    const serialized = serialize(attrs);
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => {
+		return client.hGetAll(itemsKey(id));
+	});
 
-    await client.hSet(itemsKey(id), serialized);
+	const results = await Promise.all(commands);
 
-    return id;
+	return results.map((result, i) => {
+		if (Object.keys(result).length === 0) {
+			return null;
+		}
+
+		return deserialize(ids[i], result);
+	});
+};
+
+export const createItem = async (attrs: CreateItemAttrs) => {
+	const id = genId();
+
+	const serialized = serialize(attrs);
+
+	await client.hSet(itemsKey(id), serialized);
+
+	return id;
 };
