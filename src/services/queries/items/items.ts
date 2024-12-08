e8f1766d066a5b4967bed1ef500d5b66af4b3255
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-08 18:05:16 +0900

    feat: 조회 수 체크를 위해 아이템 생성 시 정렬집합 생성
    Promise를 통해 파이프 라인으로 한번에 동시 명령을 수행하게 만듬

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
-import { itemsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
 import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {
@@ -36,7 +36,12 @@ export const createItem = async (attrs: CreateItemAttrs) => {
 
 	const serialized = serialize(attrs);
 
-	await client.hSet(itemsKey(id), serialized);
-
+	await Promise.all([
+		await client.hSet(itemsKey(id), serialized),
+		await client.zAdd(itemsByViewsKey(), {
+			value: id,
+			score: 0
+		})
+	])
 	return id;
 };
