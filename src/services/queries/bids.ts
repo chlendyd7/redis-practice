a4385237aee619a9b5e5bf45fc59ec5f2f941e4e
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-18 22:09:29 +0900

    feat: lock을 이용한 트랜잭션 처리

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,12 +1,40 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { bidHistoryKey, itemsKey, itemsByPriceKey } from '$services/keys';
-import { client } from '$services/redis';
+import { client, withLock } from '$services/redis';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
 	return withLock(attrs.itemId, async () => {
-		
+		const item = await getItem(attrs.itemId);
+
+		if (!item) {
+			throw new Error('Item does not exist');
+		}
+		if (item.price >= attrs.amount) {
+			throw new Error('Bid too low');
+		}
+		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+			throw new Error('Item closed to bidding');
+		}
+
+		const serialized = serializeHistory(
+			attrs.amount,
+			attrs.createdAt.toMillis()
+		);
+
+		return Promise.all([
+			client.rPush(bidHistoryKey(attrs.itemId), serialized),
+			client.hSet(itemsKey(item.id), {
+				bids: item.bids + 1,
+				price: attrs.amount,
+				highestBidUserId: attrs.userId
+			}),
+			client.zAdd(itemsByPriceKey(), {
+				value: item.id,
+				score: attrs.amount
+			})
+		]);
 	});
 
 
